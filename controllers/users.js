c4faf1a4f364841704241946ef9ee0fb9ee1c732
Nspired1 <don.spire1@gmail.com>
2021-04-19 14:44:34 -0700

    format & lint rules refactor

--- controllers/users.js
+++ controllers/users.js
@@ -1,40 +1,40 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
 module.exports.renderRegisterForm = (req, res) => {
-    res.render('users/register');
-}
+  res.render("users/register");
+};
 
-module.exports.register = async(req, res, next) => {
-    try{
-        const { email, username, password, id } = req.body;
-        const user = new User({ email, username, id});
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, error => {
-            if(error) return next(error);
-            req.flash("success", "Welcome to Cal Parks");
-            res.redirect("/parks");
-        })
-    } catch(e){
-        req.flash('error', e.message);
-        res.redirect('register');
-    }
-}
+module.exports.register = async (req, res, next) => {
+  try {
+    const { email, username, password, id } = req.body;
+    const user = new User({ email, username, id });
+    const registeredUser = await User.register(user, password);
+    req.login(registeredUser, (error) => {
+      if (error) return next(error);
+      req.flash("success", "Welcome to Cal Parks");
+      res.redirect("/parks");
+    });
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("register");
+  }
+};
 
 module.exports.renderLoginForm = (req, res) => {
-    res.render('users/login');
-}
+  res.render("users/login");
+};
 
 // technically passport does the logging in, this is more redirecting and rendering, but for all intents and purposes
 // this is the login route.
 module.exports.login = (req, res) => {
-    req.flash('success', 'Welcome Back!');
-    const redirectUrl = req.session.returnTo || '/parks';
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
-}
+  req.flash("success", "Welcome Back!");
+  const redirectUrl = req.session.returnTo || "/parks";
+  delete req.session.returnTo;
+  res.redirect(redirectUrl);
+};
 
 module.exports.logout = (req, res) => {
-    req.logout();
-    req.flash('sucess', 'Goodbye!');
-    res.redirect('/parks');
-}
+  req.logout();
+  req.flash("sucess", "Goodbye!");
+  res.redirect("/parks");
+};
--- middleware.js
+++ middleware.js
@@ -1,59 +1,59 @@
 const Park = require("./models/park");
 const Review = require("./models/review");
 const ExpressError = require("./utils/ExpressError");
-const parkJoiSchema = require('./utils/parkJoiSchema');
-const reviewJoiSchema = require('./utils/reviewJoiSchema');
+const parkJoiSchema = require("./utils/parkJoiSchema");
+const reviewJoiSchema = require("./utils/reviewJoiSchema");
 
 module.exports.isLoggedIn = (req, res, next) => {
-    if(!req.isAuthenticated()){
-        req.session.returnTo = req.originalUrl;
-        req.flash("error", "You must be signed in first.");
-        return res.redirect("/login");
-    }
-    next();
-}
+  if (!req.isAuthenticated()) {
+    req.session.returnTo = req.originalUrl;
+    req.flash("error", "You must be signed in first.");
+    return res.redirect("/login");
+  }
+  next();
+};
 
 //-- validations middleware with Joi --//
 module.exports.validatePark = (req, res, next) => {
-    //parkJoiSchema is the Joi schema for park validation
-     const { error } = parkJoiSchema.validate(req.body);
-     if(error){
-         const msg = error.details.map(element => element.message).join(',')
-         throw new ExpressError(msg, 400)
-     } else {
-         next();
-     }
- }
+  //parkJoiSchema is the Joi schema for park validation
+  const { error } = parkJoiSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((element) => element.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
 
 //-- validation middleware for author of a PARK --//
-module.exports.isAuthor = async(req, res, next) => {
-    const {id} = req.params;
-    const park = await Park.findById(id);
-    if(!park.author.equals(req.user._id)){
-        req.flash('error', "You don't have permission to do that");
-        return res.redirect('/parks/${id}');
-    }
-    next();
- }
+module.exports.isAuthor = async (req, res, next) => {
+  const { id } = req.params;
+  const park = await Park.findById(id);
+  if (!park.author.equals(req.user._id)) {
+    req.flash("error", "You don't have permission to do that");
+    return res.redirect("/parks/${id}");
+  }
+  next();
+};
 
 //-- validation middleware for author of a REVIEW --//
- module.exports.isReviewAuthor = async(req, res, next) => {
-    const {id, reviewId} = req.params;
-    const review = await Review.findById(reviewId);
-    if(!review.author.equals(req.user._id)){
-        req.flash('error', "You don't have permission to do that");
-        return res.redirect('/parks/${id}');
-    }
-    next();
- }
+module.exports.isReviewAuthor = async (req, res, next) => {
+  const { id, reviewId } = req.params;
+  const review = await Review.findById(reviewId);
+  if (!review.author.equals(req.user._id)) {
+    req.flash("error", "You don't have permission to do that");
+    return res.redirect("/parks/${id}");
+  }
+  next();
+};
 
- //-- validations middleware with Joi --//
+//-- validations middleware with Joi --//
 module.exports.validateReview = (req, res, next) => {
-    const { error } = reviewJoiSchema.validate(req.body);
-    if(error){
-        const msg = error.details.map(element => element.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
+  const { error } = reviewJoiSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((element) => element.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
