fa3289436bb73690d03862208b3fc5eea7909fea
Nspired1 <don.spire1@gmail.com>
2021-04-20 23:09:01 -0700

    formatted & linted

--- models/user.js
+++ models/user.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require('passport-local-mongoose');
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const userSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    }
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
 });
 
 // don't need username and password for User Schema because
 // plugin for passportLocalMongoose saves username and password.
 userSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
